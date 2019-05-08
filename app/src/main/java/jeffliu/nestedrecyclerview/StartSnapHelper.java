b7dca8b63a04e1dcc4f652eb890dd4304e959da4
Jeffrey Liu <jeffreyliu8@gmail.com>
2019-05-07 17:36:24 -0700

    update to androidx

--- app/src/main/java/jeffliu/nestedrecyclerview/StartSnapHelper.java
+++ app/src/main/java/jeffliu/nestedrecyclerview/StartSnapHelper.java
@@ -1,11 +1,11 @@
 package jeffliu.nestedrecyclerview;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.LinearSnapHelper;
-import android.support.v7.widget.OrientationHelper;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 
 public class StartSnapHelper extends LinearSnapHelper {
