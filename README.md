04830a3195d8507abd9c53519962d210c3a050f2
Jeffrey Liu <jeffrey.liu@zoom.us>
2022-05-26 09:26:48 -0700

    Update from recyclerview to jetpack compose

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Nested-RecyclerView
-A simple horizontal recyclerView inside vertical recyclerView
+# Nested-RecyclerView(now compose)
+A simple horizontal recyclerView(in old commit) inside vertical recyclerView
+
+WARNING! starting on May 26 2022, the mainline branch code has been updated from recyclerview to Jetpack compose!
+
+It's easier with compose, please use it.
 
 ![Output sample](https://github.com/jeffreyliu8/Nested-RecyclerView/blob/master/preview.gif)
 
--- app/src/main/java/jeffliu/nestedrecyclerview/MainActivity.kt
+++ app/src/main/java/jeffliu/nestedrecyclerview/MainActivity.kt
@@ -2,28 +2,32 @@ package jeffliu.nestedrecyclerview
 
 import android.os.Bundle
 import android.os.StrictMode
-import android.view.View
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jeffliu.nestedrecyclerview.ui.theme.MyApplicationxxxTheme
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        setSupportActionBar(toolbar)
 
         turnOnStrictMode()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         val subList1 = ArrayList<Int>()
         for (i in 0..49) {
             subList1.add(i)
@@ -33,30 +37,73 @@ class MainActivity : AppCompatActivity() {
             list.add(subList1)
         }
 
-        recyclerView.setHasFixedSize(true)
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
-        val adapter = OuterAdapter(object : InnerAdapter.OnItemClickListener {
-            override fun onItemClick(view: View, data: Int) {
-                Toast.makeText(this@MainActivity, "click $data", Toast.LENGTH_SHORT).show()
+        setContent {
+            MyApplicationxxxTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NestedLists(list)
+                }
             }
-
-            override fun onItemLongClick(view: View, data: Int) {
-                Toast.makeText(this@MainActivity, "long click $data", Toast.LENGTH_SHORT).show()
-            }
-        })
-        recyclerView.adapter = adapter
-        adapter.updateList(list)
+        }
     }
 
     private fun turnOnStrictMode() {
         StrictMode.setThreadPolicy(
-                StrictMode.ThreadPolicy.Builder().detectAll()
-                        .penaltyLog().penaltyFlashScreen().build()
+            StrictMode.ThreadPolicy.Builder().detectAll()
+                .penaltyLog().penaltyFlashScreen().build()
         )
         StrictMode.setVmPolicy(
-                StrictMode.VmPolicy.Builder().detectAll()
-                        .penaltyLog().build()
+            StrictMode.VmPolicy.Builder().detectAll()
+                .penaltyLog().build()
         )
+    }
+}
+
+
+@Composable
+fun NestedLists(nestedList: List<List<Int>>) {
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(nestedList) { item ->
+            ComposeRow(item)
+        }
+    }
+}
+
+@Composable
+fun ComposeRow(list: List<Int>) {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(list) { item ->
+            Surface(
+                shadowElevation = 8.dp,
+                shape = RoundedCornerShape(8.dp),
+            ) {
+                Text(text = item.toString(), modifier = Modifier.padding(16.dp))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    val subList1 = ArrayList<Int>()
+    for (i in 0..49) {
+        subList1.add(i)
+    }
+    val list = ArrayList<ArrayList<Int>>()
+    for (i in 0..49) {
+        list.add(subList1)
+    }
+    MyApplicationxxxTheme {
+        NestedLists(list)
     }
 }
